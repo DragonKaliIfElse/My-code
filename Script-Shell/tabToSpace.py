e0353e0b3896cdf917ddbc090ee2df70087b488b
DragonKaliIfElse <dudu.vom@gmail.com>
2024-08-25 02:34:16 -0300

    addition of geolocation to the repo, and new way to catch args

--- Script-Shell/tabToSpace.py
+++ Script-Shell/tabToSpace.py
@@ -2,11 +2,11 @@
 import sys
 
 def main():
-	arquivo = sys.argv[1]
-	with open(arquivo, 'r') as file:
-		script = file.read()
-	with open(arquivo, 'w') as file:
-		script = script.replace("	","    ")
-		file.write(script)
+    arquivo = sys.argv[1]
+    with open(arquivo, 'r') as file:
+        script = file.read()
+    with open(arquivo, 'w') as file:
+        script = script.replace("    ","    ")
+        file.write(script)
 
 if __name__=='__main__':main();
