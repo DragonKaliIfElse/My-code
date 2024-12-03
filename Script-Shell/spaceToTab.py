aabeb37890b8d669d02b4076716381f2bcf7c678
DragonKaliIfElse <dudu.vom@gmail.com>
2024-12-03 13:34:39 -0300

    atualization

--- Script-Shell/spaceToTab.py
+++ Script-Shell/spaceToTab.py
@@ -2,11 +2,11 @@
 import sys
 
 def main():
-	arquivo = sys.argv[1]
-	with open(arquivo, 'r') as file:
-		editavel = file.read()
-	with open(arquivo, 'w') as file:
-		editavel = editavel.replace('    ','	')
-		file.write(editavel)
+    arquivo = sys.argv[1]
+    with open(arquivo, 'r') as file:
+        editavel = file.read()
+    with open(arquivo, 'w') as file:
+        editavel = editavel.replace('    ','	')
+        file.write(editavel)
 
 if __name__=="__main__": main();
