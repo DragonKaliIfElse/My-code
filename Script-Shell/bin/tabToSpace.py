721ba1b0c6db23321c1dbbcc33cc4ca1ed9adf70
DragonKaliIfElse <dudu.vom@gmail.com>
2024-12-03 13:31:38 -0300

    atualization

---
+++ Script-Shell/bin/tabToSpace.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+import sys
+
+def main():
+	arquivo = sys.argv[1]
+	with open(arquivo, 'r') as file:
+		script = file.read()
+	with open(arquivo, 'w') as file:
+		script = script.replace("	","    ")
+		file.write(script)
+
+if __name__=='__main__':main();
